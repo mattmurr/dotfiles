d036bd0a720ad49da9f9af323c1f89d0828ab7c1
Matthew Murray <elegant.dusk3770@fastmail.com>
2024-01-10 21:15:05 +0000

    Initial commit from arch

---
+++ .config/nvim/ftplugin/java.lua
@@ -0,0 +1,60 @@
+local home = vim.env.HOME
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace_dir = home .. '/.local/share/eclipse/' .. project_name
+local mason = home .. '/.local/share/nvim/mason'
+local jdtls = mason .. '/packages/jdtls'
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local jdtls_capabilities = require 'jdtls'.extendedClientCapabilities
+
+local config = {
+  cmd = {
+    home .. '/.jenv/versions/17.0/bin/java',
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xms1g',
+    '-Xmx8g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+    '-javaagent:' .. jdtls .. '/lombok.jar=ECJ',
+    '-jar', vim.fn.glob(jdtls .. '/plugins/org.eclipse.equinox.launcher_*.jar', 1),
+    '-configuration', jdtls .. '/config_mac',
+    '-data', workspace_dir
+  },
+  root_dir = vim.fs.dirname(vim.fs.find({ '.gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+  settings = {
+    java = {
+      signatureHelp = { enabled = true };
+      configuration = {
+        runtimes = {
+          {
+            name = 'JavaSE-17',
+            path = vim.env.HOME .. '/.jenv/versions/17.0',
+            default = true
+          },
+          {
+            name = 'JavaSE-1.8',
+            path = vim.env.HOME .. '/.jenv/versions/1.8',
+          }
+        }
+      },
+      format = {
+        settings = {
+          url = 'https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml',
+          profile = 'GoogleStyle'
+        }
+      }
+    },
+  },
+
+  capabilities = vim.tbl_deep_extend("keep", capabilities, jdtls_capabilities)
+}
+-- Disable echo for loading, we use fidget.nvim
+config.handlers = {['language/status'] = function() end}
+
+require('jdtls').start_or_attach(config)
+require('jdtls.setup').add_commands()
---
+++ .config/nvim/init.lua
@@ -0,0 +1,415 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 2
+vim.o.expandtab = true
+
+vim.o.ignorecase = true
+vim.o.incsearch = false
+vim.o.hlsearch = true
+vim.o.updatetime = 300
+vim.o.incsearch = false
+vim.wo.signcolumn = "yes"
+vim.cmd([[set mouse=]])
+
+local opts = { silent = true, noremap = true }
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "<space>de", vim.diagnostic.setloclist, opts)
+vim.keymap.set("n", "<space>we", vim.diagnostic.setqflist, opts)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+
+require("lazy").setup({
+	{ "folke/neodev.nvim", opts = {} },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				auto_install = true,
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = { "markdown" },
+				},
+			})
+		end,
+	},
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.4',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function()
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>t', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+			vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+			vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
+			vim.keymap.set('n', '<leader>a', builtin.treesitter, {})
+
+			require("telescope").setup {
+				defaults = {
+					path_display = { "smart" },
+					preview = {
+						filesize_limit = 1.0, -- MB
+					},
+					vimgrep_arguments = {
+						"rg",
+						"--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case",
+						"--trim" -- add this value
+					},
+					mappings = {
+						i = {
+							["<C-u>"] = false
+						},
+					},
+				},
+				pickers = {
+					find_files = {
+						find_command = { "fd", "--type", "f", "--strip-cwd-prefix" }
+					},
+				}
+			}
+		end
+	},
+	{
+		"toppair/peek.nvim",
+		event = { "VeryLazy" },
+		build = "deno task --quiet build:fast",
+		config = function()
+			require("peek").setup()
+			-- refer to `configuration to change defaults`
+			vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+		end,
+	},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					-- default options: exact mode, multi window, all directions, with a backdrop
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "o", "x" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+					require("flash").jump({
+						pattern = ".", -- initialize pattern with any char
+						search = {
+							mode = function(pattern)
+								-- remove leading dot
+								if pattern:sub(1, 1) == "." then
+									pattern = pattern:sub(2)
+								end
+								-- return word pattern and proper skip pattern
+								return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
+							end,
+						},
+						-- select the range
+						jump = { pos = "range" },
+					})
+				end,
+				desc = "Remote Flash",
+			},
+		},
+	},
+	{
+		"RRethy/nvim-base16",
+		config = function()
+			vim.cmd("colorscheme base16-da-one-black")
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "UiEnter",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				theme = "base16",
+			})
+		end,
+	},
+	{
+		"utilyre/barbecue.nvim",
+		name = "barbecue",
+		version = "*",
+		dependencies = {
+			"SmiteshP/nvim-navic",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("barbecue").setup()
+		end,
+	},
+	"preservim/nerdcommenter",
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({})
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		config = function()
+			require("todo-comments").setup({})
+		end,
+	},
+	{
+		"j-hui/fidget.nvim",
+		tag = "legacy",
+		config = function()
+			require("fidget").setup({})
+		end,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {},
+		config = function()
+			require("ibl").setup({})
+		end,
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		config = function()
+			vim.g.loaded_netrw = 1
+			vim.g.loaded_netrwPlugin = 1
+			require("nvim-tree").setup({
+				view = {
+					adaptive_size = true,
+				},
+				update_focused_file = {
+					enable = true,
+				},
+				diagnostics = {
+					enable = true,
+				},
+			})
+		end,
+	},
+	{
+		"ray-x/lsp_signature.nvim",
+		config = function()
+			require("lsp_signature").setup({ zindex = 50 })
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup()
+
+			local nvim_lsp = require("lspconfig")
+			local servers = {
+				"pyright",
+				"tsserver",
+				"gopls",
+				"lua_ls",
+				"solargraph",
+				"zls",
+				"clangd",
+				"ltex",
+				"rust_analyzer",
+				"kotlin_language_server",
+				"astro",
+				"tailwindcss",
+				"prismals",
+				"rnix",
+				"lemminx",
+				"jsonls",
+				"dockerls",
+				"docker_compose_language_service",
+				"r_language_server",
+				"marksman",
+				"elixirls",
+			}
+			-- Use a loop to conveniently call 'setup' on multiple servers and
+			-- map buffer local keybindings when the language server attaches
+			for _, lsp in ipairs(servers) do
+				local opts = {
+					flags = {
+						debounce_text_changes = 150,
+					},
+				}
+				if lsp == "ltex" then
+					opts.settings = {
+						ltex = {
+							language = "en-GB",
+						},
+					}
+				elseif lsp == "lua_ls" then
+					opts.settings = {
+						Lua = {
+							diagnostics = {
+								globals = { "vim" },
+							},
+						},
+					}
+				elseif lsp == "gopls" then
+					opts.settings = {
+						gopls = {
+							codelenses = {
+								generate = true,
+								gc_details = true,
+								test = true,
+								tidy = true,
+							},
+							analyses = {
+								fieldalignment = true,
+								unusedparams = true,
+							},
+							staticcheck = true,
+							allExperiments = true,
+						},
+					}
+				end
+				nvim_lsp[lsp].setup(opts)
+			end
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					-- Enable completion triggered by <c-x><c-o>
+					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+					-- Buffer local mappings.
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+
+					---@diagnostic disable-next-line: redefined-local
+					local opts = { buffer = ev.buf }
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					vim.keymap.set('n', '<space>f', function()
+						vim.lsp.buf.format({ async = true })
+					end, opts)
+				end,
+			})
+		end,
+	},
+	"mfussenegger/nvim-jdtls",
+	"mfussenegger/nvim-dap",
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+			"onsails/lspkind.nvim",
+		},
+		config = function()
+			vim.cmd([[set completeopt=menu,menuone,noselect]])
+
+			local cmp = require("cmp")
+			local lspkind = require("lspkind")
+
+			cmp.setup({
+				formatting = {
+					format = lspkind.cmp_format({
+						maxwidth = 64,
+						mode = "symbol_text",
+						menu = {
+							buffer = "[Buffer]",
+							nvim_lsp = "[LSP]",
+							luasnip = "[LuaSnip]",
+							nvim_lua = "[Lua]",
+							latex_symbols = "[Latex]",
+						},
+					}),
+				},
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" }, -- For luasnip users.
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+
+			-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+			})
+		end,
+	},
+})
